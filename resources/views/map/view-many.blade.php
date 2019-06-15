10061d8d6516b217090fc2b3a314ad32e5156a86
Jordan Leonardi <jordantheyinz@gmail.com>
2019-06-15 12:11:40 +0700

    map + leaflet + update gitignore

---
+++ resources/views/map/view-many.blade.php
@@ -0,0 +1,31 @@
+@extends('map.template.map')
+
+@section('map-script')
+    <script>
+        $(function() {
+            var map = L.map('map').setView(['0', '0'], 16);
+            @foreach($data as $dt)
+                var marker = L.marker(['{{$dt->latitude}}', '{{$dt->longitude}}']).addTo(map);
+                marker.bindPopup('{{$dt->name}}');
+            @endforeach
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    latit = position.coords.latitude;
+                    longit = position.coords.longitude;
+                    var circle = L.circle([latit, longit], {
+                        color: 'blue',
+                        fillColor: 'blue',
+                        fillOpacity: 0.5,
+                        radius: 10
+                    }).addTo(map);
+                    circle.bindPopup('Current Location').openPopup();
+                    // var m = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+                    map.panTo(new L.LatLng(latit, longit));
+                });
+            }
+        }());
+    </script>
+@endsection
